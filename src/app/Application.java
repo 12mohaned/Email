ab766e6fb27c8bf1c5c44ea67cd4b6493124c33c
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-09-05 00:03:02 +0200

    Update Application.java

--- src/app/Application.java
+++ src/app/Application.java
@@ -2,7 +2,6 @@ package app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import authentication.Authentication;
